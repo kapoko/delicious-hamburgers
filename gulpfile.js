5f642cdb4d732c9385219a17c57a4872c9da6420
Kasper Koman <kasper.koman@gmail.com>
2017-05-04 09:40:06 +0200

    Added converge and twist animations

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     gulpSequence = require('gulp-sequence'),
     clean = require('gulp-clean'),
-    browserSync = require('browser-sync').create(),
     autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('clean', function() {
@@ -16,7 +15,7 @@ gulp.task('clean', function() {
 gulp.task('css', function() {
     return gulp.src('sass/hamburgers.scss')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(autoprefixer('last 5 versions'))
+        .pipe(autoprefixer('last 2 versions'))
         .pipe(gulp.dest('dist'))
         .pipe(rename({suffix: '.min'}))
         .pipe(sass({outputStyle: 'compressed'}))
